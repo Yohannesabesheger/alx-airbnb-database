9296ad180c39224da2048ddf7fc5fd1edf7dc79f
Yohannes Mitike Mersha <113605459+Yohannesabesheger@users.noreply.github.com>
2025-05-15 09:30:12 +0300

    Testing one by one

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,21 @@
-worked well
+-- Write a query using an INNER JOIN to retrieve all bookings and the respective users who made those bookings.
+-- Inner Join
+SELECT 
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.status,
+    b.created_at AS booking_created_at,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    u.phone_number,
+    u.role,
+    u.created_at AS user_created_at
+FROM 
+    Booking b
+INNER JOIN 
+    User u ON b.user_id = u.user_id;
+
