012b7290833e0c5099d6b263cddd707711b74777
Yohannes Mitike Mersha <113605459+Yohannesabesheger@users.noreply.github.com>
2025-05-15 09:19:30 +0300

    quary checking

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -21,21 +21,34 @@ INNER JOIN
 
 --Write a query using aLEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
 
+-- SELECT 
+--     p.property_id,
+--     p.name AS property_name,
+--     p.location,
+--     r.review_id,
+--     r.rating,
+--     r.comment,
+--     r.created_at,
+--     u.user_id,
+--     u.first_name,
+--     u.last_name
+-- FROM 
+--     Property p
+-- LEFT JOIN Review r ON p.property_id = r.property_id
+-- LEFT JOIN User u ON r.user_id = u.user_id;
+
 SELECT 
     p.property_id,
     p.name AS property_name,
     p.location,
-    r.review_id,
-    r.rating,
-    r.comment,
-    r.created_at,
-    u.user_id,
-    u.first_name,
-    u.last_name
+    COUNT(r.review_id) AS total_reviews,
+    AVG(r.rating) AS average_rating
 FROM 
     Property p
 LEFT JOIN Review r ON p.property_id = r.property_id
-LEFT JOIN User u ON r.user_id = u.user_id;
+GROUP BY 
+    p.property_id, p.name, p.location;
+
 
 -- Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a use
 
